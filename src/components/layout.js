132d1fd629d448674095f7d4eee13554885729f7
Sam Reichle <samjhreichle@gmail.com>
2020-08-27 12:03:21 -0500

    Added custom favicon and styled for mobile view

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,10 +3,12 @@ import Header from './header';
 import Footer from './footer';
 import '../styles/index.scss';
 import LayoutStyles from '../styles/layout.module.scss';
+import favicon from '../images/sr-favicon.png'
 
 const Layout = (props) => {
     return (
         <div className={LayoutStyles.container}>
+        <link rel="icon" type="image/png" href={favicon} />
             <div className={LayoutStyles.content}>
                 <Header />
                 {props.children}
