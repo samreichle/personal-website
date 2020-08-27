1703c6da3c3bb1e8ed8431e3f4ca56a1e711ffff
Sam Reichle <samjhreichle@gmail.com>
2020-08-27 09:40:07 -0500

    Made some changes.

---
+++ src/pages/404.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/layout.js';
+
+
+const ErrorPage = () => {
+    return (
+        <Layout>
+            <p>404! Error Loading Page!</p>
+        </Layout>
+    )
+}
+
+export default ErrorPage;
