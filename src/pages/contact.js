680d18e9941b180192e1c2b9423e921121ec289f
Sam Reichle <samjhreichle@gmail.com>
2020-09-04 13:16:12 -0500

    Centered Contact Info

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,11 +6,11 @@ import ContactStyles from '../styles/contact.module.scss';
 const ContactPage = () => {
     return (
         <Layout>
-            <h1>Contact Information</h1>
-            <p>Feel free to reach out if you're looking for a developer, have a question, or just want to connect.</p>
-            <p><a className={ContactStyles.link} href="mailto: samjhreichle@gmail.com" target="_blank">samjhreichle@gmail.com</a></p>
-            <p><a className={ContactStyles.link} href="https://www.linkedin.com/in/samreichle/" target="_blank">LinkedIn</a></p>
-            <p><a className={ContactStyles.link} href="https://github.com/samreichle" target="_blank">GitHub</a></p>
+                <h1 className={ContactStyles.centerContent}>Contact Information</h1>
+                <p className={ContactStyles.centerContent}>Feel free to reach out if you're looking for a developer, have a question, or just want to connect.</p>
+                <p className={ContactStyles.centerContent}><a className={ContactStyles.link} href="mailto: samjhreichle@gmail.com" target="_blank">samjhreichle@gmail.com</a></p>
+                <p className={ContactStyles.centerContent}><a className={ContactStyles.link} href="https://www.linkedin.com/in/samreichle/" target="_blank">LinkedIn</a></p>
+                <p className={ContactStyles.centerContent}><a className={ContactStyles.link} href="https://github.com/samreichle" target="_blank">GitHub</a></p>
 
         </Layout>
     )
