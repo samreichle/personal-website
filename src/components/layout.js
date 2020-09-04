55c8f8615fdc47b7a0de17ddc81cc781f074f2ef
Sam Reichle <samjhreichle@gmail.com>
2020-09-04 16:27:45 -0500

    Updated favicon

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 import Header from './header';
 import Footer from './footer';
 import '../styles/index.scss';
 import LayoutStyles from '../styles/layout.module.scss';
-import favicon from '../images/sr-favicon.png'
+import favicon from '../images/favicon.ico';
 
 const Layout = (props) => {
     return (
         <div className={LayoutStyles.container}>
-        <link rel="icon" type="image/png" href={favicon} />
+            <Helmet>
+                <link rel="icon" href={favicon} />
+            </Helmet>
             <div className={LayoutStyles.content}>
                 <Header />
                 {props.children}
